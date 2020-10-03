e6b461587f1c848ef5d10ee408c7f05520155d47
Dick Wyn Yong <dickwyn@outlook.com>
2020-10-02 22:26:30 -0700

    Updates

---
+++ precache-manifest.147c819b993045b841d36132ba1650ff.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "9412a0a86f9efae944e5",
+    "url": "/international-night-2018/static/css/main.2376e605.chunk.css"
+  },
+  {
+    "revision": "9412a0a86f9efae944e5",
+    "url": "/international-night-2018/static/js/main.9412a0a8.chunk.js"
+  },
+  {
+    "revision": "e77f867e1fe4fbf60123",
+    "url": "/international-night-2018/static/css/1.6dc20c86.chunk.css"
+  },
+  {
+    "revision": "e77f867e1fe4fbf60123",
+    "url": "/international-night-2018/static/js/1.e77f867e.chunk.js"
+  },
+  {
+    "revision": "242769638c28ad1cc27e",
+    "url": "/international-night-2018/static/js/runtime~main.24276963.js"
+  },
+  {
+    "revision": "a7fdced0391e5b5d4ca1b89cab7083e3",
+    "url": "/international-night-2018/static/media/in-globe.a7fdced0.png"
+  },
+  {
+    "revision": "921f1150167369cf4c400135a4905728",
+    "url": "/international-night-2018/static/media/fa-brands-400.921f1150.woff2"
+  },
+  {
+    "revision": "f2594ef62455697f61dc99862c19afba",
+    "url": "/international-night-2018/static/media/fa-brands-400.f2594ef6.eot"
+  },
+  {
+    "revision": "9d56249d09070f656a1c52e566448f0d",
+    "url": "/international-night-2018/static/media/fa-brands-400.9d56249d.woff"
+  },
+  {
+    "revision": "491a96d8188670aff6f5e8ae4e29ed6a",
+    "url": "/international-night-2018/static/media/fa-regular-400.491a96d8.eot"
+  },
+  {
+    "revision": "ab673bac3a9e36cb1d4e54777c413fe3",
+    "url": "/international-night-2018/static/media/fa-brands-400.ab673bac.ttf"
+  },
+  {
+    "revision": "e6ff1c5d13b7786272782029310c3615",
+    "url": "/international-night-2018/static/media/fa-regular-400.e6ff1c5d.ttf"
+  },
+  {
+    "revision": "4f8bb28722068f7b666582a39c7035b8",
+    "url": "/international-night-2018/static/media/fa-regular-400.4f8bb287.woff"
+  },
+  {
+    "revision": "cf6008d396082c09c3dd4907de9f3941",
+    "url": "/international-night-2018/static/media/fa-regular-400.cf6008d3.woff2"
+  },
+  {
+    "revision": "1dc5b6dd4bf409a6f919be38603f76a0",
+    "url": "/international-night-2018/static/media/fa-solid-900.1dc5b6dd.woff2"
+  },
+  {
+    "revision": "b5a61b229c9c92a6ac21f5b0e3c6e9f1",
+    "url": "/international-night-2018/static/media/fa-regular-400.b5a61b22.svg"
+  },
+  {
+    "revision": "20b351a6af2d523589fd193785e7d7f0",
+    "url": "/international-night-2018/static/media/fa-solid-900.20b351a6.eot"
+  },
+  {
+    "revision": "997f6b1819184c54248de33c81b7bb0c",
+    "url": "/international-night-2018/static/media/fa-solid-900.997f6b18.woff"
+  },
+  {
+    "revision": "63726a69fa60cb67459140ccaf679f96",
+    "url": "/international-night-2018/static/media/fa-solid-900.63726a69.ttf"
+  },
+  {
+    "revision": "c862e94cbef741d18838774587e3c49d",
+    "url": "/international-night-2018/static/media/fa-brands-400.c862e94c.svg"
+  },
+  {
+    "revision": "1d220cf9da36861171fa90d3c164f4d3",
+    "url": "/international-night-2018/static/media/fa-solid-900.1d220cf9.svg"
+  },
+  {
+    "revision": "b471adbe3c3368cda6435783762d7340",
+    "url": "/international-night-2018/index.html"
+  }
+];
